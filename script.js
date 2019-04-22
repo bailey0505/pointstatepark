cd740f189e71e486614f7e2dde673aea4f0427ec
bailey0505 <baileyrotellini1998@gmail.com>
2019-04-22 14:36:46 -0400

    added nav bar, cs file, js file and other pages to project

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//START NAV BAR SCRIPT///////
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+/// END NAV BAR SCRIPT ////////////
